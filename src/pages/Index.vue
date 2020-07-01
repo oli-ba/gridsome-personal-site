ad8b19a9a8c371606af8fef907e986c9491c46dc
Olivier Balaguer <oli@olivierbalaguer.com>
2020-07-01 21:41:29 +0100

    Fixed space after greeting

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -2,17 +2,18 @@
   <Layout class="bg-colorSecondary border-t-8 border-solid border-colorPrimary">
     <section class="hero text-colorPrimary flex flex-col justify-center pb-0 h-full">
       <div class="md:w-2/3 md:pt-24">
+        <p v-show="hasName == false" class="leading-none mb-0">&mdash; Olivier Balaguer</p>
         <h2 class="font-display leading-tight mb-6">
           Remotely based
           UI/UX facilitator &amp;&nbsp;developer
         </h2>
         <p class="font-body text-xl mb-4">
-          <span v-show="hasName == true">Hello {{name}} 👋, I'm Olivier.</span>I help companies big and small create timeless digital touchpoints.
+          <span v-show="hasName == true">Hello {{name}} 👋, I'm Olivier. </span>I help companies big and small create timeless digital touchpoints.
           The result? Delightful experiences that leave a lasting impression.
           Want to change direction, change behaviour, or change the world? I can help out.
         </p>
         <p class="font-body text-xl mb-12 relative">
-          I'm currently refreshing the ol' site and adding new work. If you're looking for my CV, <a href="Olivier-Balaguer-CV-2020.pdf" download class="font-bold underline">you can download it here</a>. If you would like to connect you can <a href="https://www.linkedin.com/in/olivierbalaguer" class="font-bold cursor-pointer underline">find me on LinkedIn</a>, or you can <a
+          I'm currently refreshing the ol' site and adding new work. If you're looking for my CV, you can <a href="Olivier-Balaguer-CV-2020.pdf" download class="font-bold underline">download it here</a>. If you would like to connect you can <a href="https://www.linkedin.com/in/olivierbalaguer" class="font-bold cursor-pointer underline">find me on LinkedIn</a>, or you can <a
             v-clipboard:copy="copyEmail"
             v-clipboard:success="onCopy"
             v-clipboard:error="onError"
@@ -60,7 +61,7 @@ import Testimonial from "~/components/Testimonial.vue";
 
 export default {
   metaInfo: {
-    title: "UX/UI Designer Based in London | Olivier Balaguer",
+    title: "Remote UI/UX Facilitator & Developer Based in London",
     link: [
       {
         rel: "canonical",
